0f65b15b177ef9962f3223f308067b2ae51bda91
ClementPla <clement.playout@polymtl.ca>
2025-04-23 15:26:45 -0400

    Update the UI and the requirements to prepare for docker containarization

---
+++ scripts/train.sh
@@ -0,0 +1,11 @@
+# python scripts/train.py --arch unetplusplus --encoder se_resnet50
+
+python scripts/train.py --arch segformer --encoder mit_b4
+python scripts/train.py --arch segformer --encoder mit_b2
+# python scripts/train.py --arch segformer --encoder mit_b0
+# # python scripts/train.py --arch unet --encoder resnet34
+python scripts/train.py --arch unet --encoder se_resnet50
+# python scripts/train.py --arch segformer --encoder se_resnet50
+# # python scripts/train.py --arch dpt --encoder tu-vit_tiny_patch16_224.augreg_in21k
+# # python scripts/train.py --arch dpt --encoder tu-vit_small_patch16_224.augreg_in21k
+# # 

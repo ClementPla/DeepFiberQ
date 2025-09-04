6e747008570fb7bc5a0455e849b09cf08732bcb8
ClementPla <clement.playout@polymtl.ca>
2025-09-04 13:36:39 -0400

    Fix the batch proces

--- dnafiber/ui/pages/2_Viewer.py
+++ dnafiber/ui/pages/2_Viewer.py
@@ -25,7 +25,7 @@ st.markdown(
     """
         <style>
                .block-container {
-                    padding-top: 2rem;
+                    padding-top: 3rem;
                     padding-bottom: 0rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
--- dnafiber/ui/pages/3_Analysis.py
+++ dnafiber/ui/pages/3_Analysis.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import torch
 from dnafiber.ui.utils import get_multifile_image, get_image_cacheless
-from dnafiber.deployment import MODELS_ZOO
+from dnafiber.deployment import MODELS_ZOO, MODELS_ZOO_R, ENSEMBLE
 import pandas as pd
 import plotly.express as px
 from dnafiber.postprocess import refine_segmentation
+
 import torch.nn.functional as F
 from joblib import Parallel, delayed
 import time
 from catppuccin import PALETTE
-from dnafiber.deployment import _get_model
-from dnafiber.ui.inference import ui_inference_cacheless
+from dnafiber.ui.inference import ui_inference_cacheless, get_model
 from dnafiber.ui.components import show_fibers_cacheless, table_components
 
 
@@ -43,7 +43,7 @@ def plot_result(seleted_category):
     show_points = st.checkbox(
         "Show points",
         value=False,
-        help="Show individual data points in the violin plot",
+        help="Show a swarm plot next to the distribution plot",
     )
     normalize = st.checkbox(
         "Normalize ratios",
@@ -108,17 +108,17 @@ def plot_result(seleted_category):
 
     palette = [c.hex for c in PALETTE.latte.colors]
 
-    fig = px.violin(
+    fig = px.box(
         clean_df,
         y="Ratio",
         x="Image",
         color="Image",
-        box=True,  # draw box plot inside the violin
         points="all" if show_points else "outliers",  # can be 'outliers', or False
         color_discrete_sequence=palette,
         log_y=True,  # Set y-axis to log scale
         range_y=[0.125 / 2, 16],
     )
+
     # Set y-axis ticks to log2 scale
     fig.update_yaxes(
         tickvals=[0.25, 0.5, 1, 2, 4, 8],
@@ -132,7 +132,7 @@ def plot_result(seleted_category):
     )
 
 
-def run_one_file(file, model, use_tta):
+def run_one_file(file, model, use_tta, use_correction=True):
     if isinstance(file, tuple):
         if file[0] is None:
             filename = file[1].name
@@ -144,69 +144,30 @@ def run_one_file(file, model, use_tta):
         image = get_image_cacheless(
             file, st.session_state.get("reverse_channels", False)
         )
-    start = time.time()
-    prediction = ui_inference_cacheless(
+    results = ui_inference_cacheless(
         _model=model,
         _image=image,
         _device="cuda" if torch.cuda.is_available() else "cpu",
-        only_segmentation=True,
+        prediction_threshold=1 / 3,
+        pixel_size=st.session_state.get("pixel_size", 0.13),
         use_tta=use_tta,
+        use_correction=use_correction,
     )
-    print(f"Prediction time: {time.time() - start:.2f} seconds for {file.name}")
-    h, w = prediction.shape
-    start = time.time()
-    y_size, x_size = 4096, 4096
-    if h > y_size or w > x_size:
-        # Extract blocks from the prediction
-
-        blocks = [
-            (
-                image[y : y + y_size, x : x + x_size],
-                prediction[y : y + y_size, x : x + x_size],
-                y,
-                x,
-            )
-            for y in range(0, h, y_size)
-            for x in range(0, w, x_size)
-        ]
-
-        parallel_results = Parallel(n_jobs=8, backend="threading")(
-            delayed(refine_segmentation)(
-                block_img,
-                block,
-                0,  # threshold
-                x,
-                y,
-            )
-            for (block_img, block, y, x) in (blocks)
-        )
-
-        results = [fiber for block_result in parallel_results for fiber in block_result]
-    else:
-        results = refine_segmentation(
-            image,
-            prediction,
-            threshold=0,  # threshold,
-        )
-    print(f"Refinement time: {time.time() - start:.2f} seconds for {filename}")
-    results = [fiber for fiber in results if fiber.is_valid]
+    results = results.filtered_copy()
     df = show_fibers_cacheless(results, image, image_id=None, resolution=256)
     df["image_name"] = filename
     return df
 
 
-def run_inference(model_name, use_tta):
+def run_inference(model_name, use_tta, use_correction):
     if "ensemble" in model_name:
-        model = [
-            _ + "_finetuned" if "finetuned" in model_name else ""
-            for _ in MODELS_ZOO.values()
-            if _ != "ensemble"
-        ]
+        model = []
+        for _ in range(len(ENSEMBLE)):
+            with st.spinner(f"Loading model {_ + 1}/{len(ENSEMBLE)}..."):
+                model.append(get_model(ENSEMBLE[_]))
     else:
-        model = _get_model(
-            revision=model_name,
-            device="cuda" if torch.cuda.is_available() else "cpu",
-        )
+        with st.spinner("Loading model..."):
+            model = get_model(model_name)
 
     my_bar = st.progress(0, text="Running segmentation...")
     all_files = st.session_state.files_uploaded
@@ -220,7 +181,7 @@ def run_inference(model_name, use_tta):
         else:
             filename = file.name
         try:
-            df = run_one_file(file, model, use_tta)
+            df = run_one_file(file, model, use_tta, use_correction)
         except Exception as e:
             st.error(f"Error processing {filename}: {e}")
             continue
@@ -247,25 +208,36 @@ if st.session_state.get("files_uploaded", None):
         )
 
         with st.expander("Model", expanded=True):
+            use_ensemble = st.checkbox(
+                "Ensemble model",
+                value=False,
+                help="Use all available models to improve segmentation results.",
+            )
             model_name = st.selectbox(
                 "Select a model",
-                list(MODELS_ZOO.keys()),
+                list(MODELS_ZOO.values()),
+                format_func=lambda x: MODELS_ZOO_R[x],
                 index=0,
                 help="Select a model to use for inference",
+                disabled=use_ensemble,
             )
-            use_tta = st.checkbox(
-                "Use TTA (Test Time Augmentation)",
-                value=True,
-                help="Use Test Time Augmentation to improve segmentation results",
-            )
-            col1, col2 = st.columns(2)
-            with col1:
-                st.write("Running on:")
-            with col2:
-                st.button(
-                    "GPU" if torch.cuda.is_available() else "CPU",
-                    disabled=True,
+            if use_ensemble:
+                st.warning(
+                    "Ensemble model is selected. All available models will be used for inference."
                 )
+                model_name = "ensemble"
+
+            use_tta = st.checkbox(
+                "Use test time augmentation (TTA)",
+                value=False,
+                help="Use test time augmentation to improve segmentation results.",
+            )
+
+            detect_errors = st.checkbox(
+                "Use error detection and filtering",
+                value=True,
+                help="Use the error filtering model to improve detection results.",
+            )
 
     tab_segmentation, tab_charts = st.tabs(["Segmentation", "Charts"])
 
@@ -274,8 +246,9 @@ if st.session_state.get("files_uploaded", None):
         if run_segmentation:
             st.session_state.image_id = None
             run_inference(
-                model_name=MODELS_ZOO[model_name] + "_finetuned",
+                model_name=model_name,
                 use_tta=use_tta,
+                use_correction=detect_errors,
             )
             st.balloons()
         if st.session_state.get("results", None) is not None:
